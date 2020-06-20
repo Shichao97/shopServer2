17501e87d7c3169e86c9f03db286ea651dd82313
Petter Lynch <petterlynch@163.com>
2020-06-20 15:48:22 +0800

    bushu

--- demotest/src/main/java/com/yibee/MyApplication.java
+++ demotest/src/main/java/com/yibee/MyApplication.java
@@ -2,14 +2,21 @@ package com.yibee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
-public class MyApplication {
+public class MyApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyApplication.class, args);
-	}
- 
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
