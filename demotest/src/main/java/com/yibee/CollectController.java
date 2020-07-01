8b84701b308d7274b243b7892dfdea485f0edcf3
liushichao <liushichao@localhost>
2020-07-01 11:12:16 -0400

    ghj

--- demotest/src/main/java/com/yibee/CollectController.java
+++ demotest/src/main/java/com/yibee/CollectController.java
@@ -7,7 +7,9 @@ import java.util.Optional;
 import java.util.Properties;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
@@ -21,49 +23,93 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.yibee.entity.Collect;
 import com.yibee.entity.Goods;
+import com.yibee.entity.Member;
 
 @RestController
 @RequestMapping("/collect")
 public class CollectController {
- @Resource
- private CollectRepository repo;
+	
+	 @Resource
+	 private CollectRepository repo;
  
- @CrossOrigin(origins = "*", maxAge = 3600)
- @GetMapping(value="/getcollecticon")
- public ResponseEntity<FileSystemResource> getCollectIcon(HttpServletResponse response,@RequestParam("goodsId") Long goodsId,@RequestParam("memberId") Long memberId) {
-  Properties pp;
-  try {
-   File file;
-   pp = MyUtil.getConfProperties();
-   String savePath = pp.getProperty("collect_icon.dir");
-   
-   Optional<Collect> oc = repo.findByGoodsidAndMemberid(goodsId, memberId);
-   if(!oc.isPresent()) {
-    
-    String absolutePath = savePath+"/purple_heart.png";
-    file = new File(absolutePath);
-   }else {
-    String absolutePath = savePath+"/red_heart.png";
-    file = new File(absolutePath);
-   }
-   
-   HttpHeaders headers = new HttpHeaders();
-      headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-      headers.add("Pragma", "no-cache");
-      headers.add("Expires", "0");
-      headers.add("Last-Modified", new Date().toString()); 
-      headers.add("ETag", String.valueOf(System.currentTimeMillis()));
-      return ResponseEntity
-        .ok()
-        .headers(headers)
-        .contentLength(file.length())
-        .contentType(MediaType.IMAGE_JPEG)
-        .body(new FileSystemResource(file));
-
-  } catch (IOException e) {
-   // TODO Auto-generated catch block
-   e.printStackTrace();
-  }
-  return ResponseEntity.status(602).build();
- }
+	 @CrossOrigin(origins = "*", maxAge = 3600)
+	 @GetMapping(value="/getcollecticon")
+	 public ResponseEntity<FileSystemResource> getCollectIcon(HttpServletResponse response,@RequestParam("goodsId") Long goodsId,@RequestParam("memberId") Long memberId) {
+	  Properties pp;
+	  try {
+	   File file;
+	   pp = MyUtil.getConfProperties();
+	   String savePath = pp.getProperty("collect_icon.dir");
+	   
+	   Optional<Collect> oc = repo.findByGoodsidAndMemberid(goodsId, memberId);
+	   if(!oc.isPresent()) {
+	    
+	    String absolutePath = savePath+"/purple_heart.png";
+	    file = new File(absolutePath);
+	   }else {
+	    String absolutePath = savePath+"/red_heart.png";
+	    file = new File(absolutePath);
+	   }
+	   
+	   HttpHeaders headers = new HttpHeaders();
+	      headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+	      headers.add("Pragma", "no-cache");
+	      headers.add("Expires", "0");
+	      headers.add("Last-Modified", new Date().toString()); 
+	      headers.add("ETag", String.valueOf(System.currentTimeMillis()));
+	      return ResponseEntity
+	        .ok()
+	        .headers(headers)
+	        .contentLength(file.length())
+	        .contentType(MediaType.IMAGE_JPEG)
+	        .body(new FileSystemResource(file));
+	
+	  } catch (IOException e) {
+	   // TODO Auto-generated catch block
+	   e.printStackTrace();
+	  }
+	  return ResponseEntity.status(602).build();
+	 }
+	 
+	 @CrossOrigin(origins = "*", maxAge = 3600)
+	 @GetMapping(value="/edit/clickcollect")
+	 public Properties clickCollect(HttpServletRequest request,@RequestParam("goodsId") Long goodsId,@RequestParam("memberId") Long memberId) {
+		 Properties p = new Properties();
+		 HttpSession session = request.getSession();
+		 Object o = session.getAttribute(MyUtil.ATTR_LOGIN_NAME);
+		 Member m =(Member)o;
+		 if(m.getId()==null || !m.getId().equals(memberId)) {
+			 p.put("success", 0);
+			 p.put("msg", "You do not have previllege to do so!");
+		 }
+		 Optional<Collect> oc = repo.findByGoodsidAndMemberid(goodsId, memberId);
+		 if(!oc.isPresent()) {
+			Collect c = oc.get();
+			try {
+				repo.deleteById(c.getId());
+				p.put("success", 1);
+			}catch(Exception e){
+				e.printStackTrace();
+				p.put("success", 0);
+				p.put("msg","Delete collection failed!");
+			}
+		    
+		    
+		 }else {
+			 Collect c = new Collect();
+			 c.setGoodsId(goodsId);
+			 c.setMemberId(memberId);
+			 try{
+				 repo.save(c);
+				 p.put("success", 1);
+			 }catch(Exception e){
+				 e.printStackTrace();
+				 p.put("success", 0);
+				 p.put("msg","Add collection failed!");
+			 }
+		 }
+		 
+		 return p;
+	 }
+ 
 }
