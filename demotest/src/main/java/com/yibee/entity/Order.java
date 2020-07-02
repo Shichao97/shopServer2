7980e55b6c0e669957882f81675ad3548a025952
liushichao <liushichao@localhost>
2020-07-01 22:22:21 -0400

    order

---
+++ demotest/src/main/java/com/yibee/entity/Order.java
@@ -0,0 +1,143 @@
+package com.yibee.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+import java.math.BigInteger;
+
+
+/**
+ * The persistent class for the orders database table.
+ * 
+ */
+@Entity
+@Table(name="orders")
+@NamedQuery(name="Order.findAll", query="SELECT o FROM Order o")
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+
+	@Column(name="buyer_id")
+	private Long buyerId;
+
+	@Column(name="buyer_name")
+	private String buyerName;
+
+	@Column(name="goods_id")
+	private BigInteger goodsId;
+
+	@Column(name="order_price")
+	private float orderPrice;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="order_time")
+	private Date orderTime;
+
+	@Column(name="payment_status")
+	private int paymentStatus;
+
+	@Column(name="receive_addr")
+	private String receiveAddr;
+
+	@Column(name="seller_id")
+	private Long sellerId;
+
+	@Column(name="seller_name")
+	private String sellerName;
+
+	private int status;
+
+	public Order() {
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getBuyerId() {
+		return this.buyerId;
+	}
+
+	public void setBuyerId(Long buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	public String getBuyerName() {
+		return this.buyerName;
+	}
+
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+
+	public BigInteger getGoodsId() {
+		return this.goodsId;
+	}
+
+	public void setGoodsId(BigInteger goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public float getOrderPrice() {
+		return this.orderPrice;
+	}
+
+	public void setOrderPrice(float orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+	public Date getOrderTime() {
+		return this.orderTime;
+	}
+
+	public void setOrderTime(Date orderTime) {
+		this.orderTime = orderTime;
+	}
+
+	public int getPaymentStatus() {
+		return this.paymentStatus;
+	}
+
+	public void setPaymentStatus(int paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	public String getReceiveAddr() {
+		return this.receiveAddr;
+	}
+
+	public void setReceiveAddr(String receiveAddr) {
+		this.receiveAddr = receiveAddr;
+	}
+
+	public Long getSellerId() {
+		return this.sellerId;
+	}
+
+	public void setSellerId(Long sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public String getSellerName() {
+		return this.sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public int getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+}
