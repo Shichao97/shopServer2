10a394c5c628aed8da41a1fe32f7de8c972df992
Petter Lynch <petterlynch@163.com>
2020-07-03 09:03:49 +0800

    NameOrder and orderRepository

--- demotest/src/main/java/com/yibee/OrderRepository.java
+++ demotest/src/main/java/com/yibee/OrderRepository.java
@@ -1,9 +1,26 @@
 package com.yibee;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.yibee.entity.Order;
+import com.yibee.entity.NameOrder;
 
 public interface OrderRepository extends PagingAndSortingRepository<Order,Long>{
+//	@Query(value = "select new com.yibee.entity.NameOrder(g.id,g.name,o.id) from Goods g  inner join Order o where g.id = o.buyerId",
+//			countQuery=" select count(g.name)  from Goods g  inner join Order o where g.id = o.buyerId")
+//	Page<NameOrder> findNameOrder(Pageable pageable);
+
+	
+	
+	@Query(value = "select new com.yibee.entity.NameOrder(g.id,g.name,o.id,o.buyerId,o.buyerName,o.sellerId,o.sellerName,o.paymentStatus,o.status,o.orderTime,o.orderPrice,o.receiveAddr) from Goods g  inner join Order o where g.id = o.buyerId",
+			countQuery=" select count(g.name)  from Goods g  inner join Order o where g.id = o.buyerId")
+	Page<NameOrder> findNameOrder(Pageable pageable);
+
+	@Query(value = "select new com.yibee.entity.NameOrder(g.id,g.name,o.id,o.buyerId,o.buyerName,o.sellerId,o.sellerName,o.paymentStatus,o.status,o.orderTime,o.orderPrice,o.receiveAddr) from Goods g  inner join Order o where g.id = o.buyerId and g.name like ?1",
+			countQuery=" select count(g.name)  from Goods g  inner join Order o where g.id = o.buyerId and g.name like ?1")
+	Page<NameOrder> findNameOrderLikeName(String name,Pageable pageable);
 
 }
---
+++ demotest/src/main/java/com/yibee/entity/NameOrder.java
@@ -0,0 +1,184 @@
+package com.yibee.entity;
+
+import java.util.Date;
+
+
+
+public class NameOrder {
+	private static final long serialVersionUID = 1L;
+	private String goodsName;
+	private Long id;
+
+	private Long buyerId;
+	private String buyerName;
+
+	private Long goodsId;
+
+	private float orderPrice;
+
+	private Date orderTime;
+
+	private int paymentStatus;
+
+	private String receiveAddr;
+
+	private Long sellerId;
+
+	private String sellerName;
+
+	private int status;
+
+	
+	public NameOrder() {
+		
+	}
+	
+	public NameOrder(		
+			String goodsName
+			) 
+	{
+		this.goodsName = goodsName;
+	}
+	
+	
+	public NameOrder(		
+			Long goodsId,
+			String goodsName,
+			Long id
+			) 
+	{
+		this.goodsId = goodsId;
+		this.goodsName = goodsName;
+		this.id = id;
+		
+	}
+
+	public NameOrder(
+		Long goodsId,
+		String goodsName,
+		Long id,
+		Long buyerId,
+		String buyerName,
+		Long sellerId,
+		String sellerName,
+		int paymentStatus,
+		int status,
+		java.util.Date orderTime,
+		float orderPrice,
+		String receiveAddr
+		) 
+	{
+			this.goodsId = goodsId;
+			this.goodsName = goodsName;
+			this.id = id;
+			this.buyerId = buyerId;
+			this.buyerName = buyerName;
+			this.sellerId = sellerId;
+			this.sellerName = sellerName;
+			this.paymentStatus = paymentStatus;
+			this.status = status;
+			this.orderTime = orderTime;
+			this.orderPrice = orderPrice;
+			this.receiveAddr = receiveAddr;
+	}		
+	
+	
+	public String getGoodsName() {
+		return goodsName;
+	}
+
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(Long buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	public String getBuyerName() {
+		return buyerName;
+	}
+
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+
+	public Long getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(Long goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public float getOrderPrice() {
+		return orderPrice;
+	}
+
+	public void setOrderPrice(float orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+	public Date getOrderTime() {
+		return orderTime;
+	}
+
+	public void setOrderTime(Date orderTime) {
+		this.orderTime = orderTime;
+	}
+
+	public int getPaymentStatus() {
+		return paymentStatus;
+	}
+
+	public void setPaymentStatus(int paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	public String getReceiveAddr() {
+		return receiveAddr;
+	}
+
+	public void setReceiveAddr(String receiveAddr) {
+		this.receiveAddr = receiveAddr;
+	}
+
+	public Long getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(Long sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+
+	
+}
