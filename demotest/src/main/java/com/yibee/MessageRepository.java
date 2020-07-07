162a9f4456a9a704224fedeace21d74077916bf2
Petter Lynch <petterlynch@163.com>
2020-07-07 21:23:46 +0800

    dd

--- demotest/src/main/java/com/yibee/MessageRepository.java
+++ demotest/src/main/java/com/yibee/MessageRepository.java
@@ -15,6 +15,6 @@ public interface MessageRepository extends PagingAndSortingRepository<Message, L
 	@Query(value = "FROM Message m where (m.toId=?1 and m.fromId=?2) or (m.toId=?2 and m.fromId=?1)")
 	Page<Message> findMessageByIDs(Long id1,Long id2,Pageable pageable);
 	
-	@Query(value = "select new com.yibee.entity.CountMessage(count(m),m.toId,m.fromId,m.fromName) FROM Message m where m.toId=?1 and m.hasRead=0 group by m.fromId order by m.sendTime desc")
+	@Query(value = "select new com.yibee.entity.CountMessage(count(m),m.toId,m.fromId,u.userName) FROM Message m left join Member u on m.fromId=u.id where m.toId=?1 and m.hasRead=0 group by m.fromId")
 	List<CountMessage> findNewCountById(Long toId);
 }
