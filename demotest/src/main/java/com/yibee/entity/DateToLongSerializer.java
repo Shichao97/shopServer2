67bec900ebedb7a19f4a925e70e64b1368bf9d30
liushichao <liushichao@localhost>
2020-07-26 09:47:09 -0400

    11

---
+++ demotest/src/main/java/com/yibee/entity/DateToLongSerializer.java
@@ -0,0 +1,16 @@
+package com.yibee.entity;
+
+import java.io.IOException;
+import java.util.Date;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+public class DateToLongSerializer extends JsonSerializer<Date> {
+    @Override
+    public void serialize(Date date, JsonGenerator jsonGenerator,
+                SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeNumber(date.getTime());
+    }
+}
